f46499a47288e201ad0f10987b860e19da9f9298
Duke <x_duke@aliyun.com>
2019-07-28 12:51:34 +0800

    New Idea: create_empty_androidapp_from_model

---
+++ idea/create_android_app_model/DefaultActivity.java
@@ -0,0 +1,13 @@
+package %PACKAGE_NAME%;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class %ACTIVITY_NAME% extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
