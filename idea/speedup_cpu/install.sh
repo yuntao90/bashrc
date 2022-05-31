7bb5a0d714fd09ab78a7e6b4ee2cab734e82993e
Duke <x_duke@aliyun.com>
2022-05-31 18:42:18 +0800

    Upgrade speedup scripts
    Upgrade install.sh and pre-install required modules
    Change the uw from 10w to 18w
    Add dynamic fan speed according to temperature

--- idea/speedup_cpu/install.sh
+++ idea/speedup_cpu/install.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+function has_command()
+{
+    local validate_command="$1"
+    local validate_result=`type -t $validate_command`
+
+    if [ -n "$validate_result" ] ; then
+        return 0
+    fi
+    return 1
+}
+
 SUDO="sudo"
 
 if [ $UID -eq 0 ] ; then
 SUDO=""
 fi
 
-MY_DIR=.
+INSTALL_PKGS="linux-tools-generic cpufrequtils acpi lm-sensors"
+
+if ! has_command jq ; then
+INSTALL_PKGS="$INSTALL_PKGS jq"
+fi
+
+$SUDO apt install  $INSTALL_PKGS
+
+MY_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 $SUDO cp -v $MY_DIR/fan_speed.conf /etc/.
 $SUDO cp -v $MY_DIR/self-speedup-cpu.service $MY_DIR/self-speedup-fan.service /lib/systemd/system/.
